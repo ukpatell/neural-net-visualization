1758d560c7b26eab6f6e66a42b585bb466968841
ukpatell <umangpatelr4@gmail.com>
2023-03-27 23:32:12 -0400

    removed tensorflow

--- api/index.py
+++ api/index.py
@@ -1,27 +1,45 @@
-from flask import Flask, render_template, request
-import tensorflow as tf
 import numpy as np
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# Sigmoid activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+# Linear activation function
+def linear(x):
+    return x
+
+
+# ReLU activation function
+def relu(x):
+    return np.maximum(0, x)
+
+
+# Tanh activation function
+def tanh(x):
+    return np.tanh(x)
+
+
 @app.route('/calculate', methods=['POST'])
 def calculate():
+    global hidden_output, output, formula
     input1 = float(request.form['input1'])
     input2 = float(request.form['input2'])
     weight1_1 = float(request.form['weight1_1'])
     weight1_2 = float(request.form['weight1_2'])
-
     weight2_1 = float(request.form['weight2_1'])
     weight2_2 = float(request.form['weight2_2'])
-
     h_weight1 = float(request.form['h_weight1'])
     h_weight2 = float(request.form['h_weight2'])
-
     bias1 = float(request.form['bias1'])
     activation = request.form['activation']
 
@@ -32,20 +50,20 @@ def calculate():
     output_biases = np.array([bias1])
 
     if activation == 'sigmoid':
-        hidden_output = tf.keras.activations.sigmoid(np.dot(inputs, hidden_weights) + hidden_biases)
-        output = tf.keras.activations.sigmoid(np.dot(hidden_output, output_weights) + output_biases)
+        hidden_output = sigmoid(np.dot(inputs, hidden_weights) + hidden_biases)
+        output = sigmoid(np.dot(hidden_output, output_weights) + output_biases)
         formula = 'sigmoid(h1*w11 + h2*w21 + b1), sigmoid(h1*w12 + h2*w22 + b2)'
     elif activation == 'tanh':
-        hidden_output = tf.keras.activations.tanh(np.dot(inputs, hidden_weights) + hidden_biases)
-        output = tf.keras.activations.tanh(np.dot(hidden_output, output_weights) + output_biases)
+        hidden_output = tanh(np.dot(inputs, hidden_weights) + hidden_biases)
+        output = tanh(np.dot(hidden_output, output_weights) + output_biases)
         formula = 'tanh(h1*w11 + h2*w21 + b1), tanh(h1*w12 + h2*w22 + b2)'
     elif activation == 'relu':
-        hidden_output = tf.keras.activations.relu(np.dot(inputs, hidden_weights) + hidden_biases)
-        output = tf.keras.activations.relu(np.dot(hidden_output, output_weights) + output_biases)
-        formula = 'max(0, h1*w11 + h2*w21 + b1), max(0, h1*w12 + h2*w22 + b2)'
+        hidden_output = relu(np.dot(inputs, hidden_weights) + hidden_biases)
+        output = relu(np.dot(hidden_output, output_weights) + output_biases)
+        formula = 'relu(h1*w11 + h2*w21 + b1), relu(h1*w12 + h2*w22 + b2)'
     elif activation == 'linear':
-        hidden_output = tf.keras.activations.linear(np.dot(inputs, hidden_weights) + hidden_biases)
-        output = tf.keras.activations.linear(np.dot(hidden_output, output_weights) + output_biases)
+        hidden_output = linear(np.dot(inputs, hidden_weights) + hidden_biases)
+        output = linear(np.dot(hidden_output, output_weights) + output_biases)
         formula = 'linear(h1*w11 + h2*w21 + b1), linear(h1*w12 + h2*w22 + b2)'
 
     h_weight1 = float(request.form['h_weight1'])
@@ -63,12 +81,12 @@ def calculate():
         'activation': activation,
         'dot_product_hidden': np.dot(inputs, hidden_weights),
         'sum_with_hidden_bias': np.dot(inputs, hidden_weights) + hidden_biases,
-        'hidden_output': hidden_output.numpy(),
-        'hidden1':hidden_output.numpy()[0],
-        'hidden2': hidden_output.numpy()[1],
+        # 'hidden_output': hidden_output.numpy(),
+        'hidden1': hidden_output[0],
+        'hidden2': hidden_output[1],
         'dot_product_output': np.dot(hidden_output, output_weights),
         'sum_with_output_bias': float(np.dot(hidden_output, output_weights) + output_biases),
-        'output': output.numpy()[0],
+        'output': output[0],
         'formula': formula
     }
 
@@ -76,7 +94,4 @@ def calculate():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
-
-
+    app.run(debug=True)
